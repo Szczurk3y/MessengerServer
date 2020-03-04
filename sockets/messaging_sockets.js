b725f41ada1484b7f42a1b2b784d3aa142ed8567
maciek stoiński <maciek.stoinski2@gmail.com>
2020-03-04 14:16:34 +0100

    Add private communication between users based on previously added chat rooms

--- sockets/messaging_sockets.js
+++ sockets/messaging_sockets.js
@@ -1,12 +1,12 @@
 module.exports = (io) => {
     io.sockets.on('connection', (socket) => {
-        console.log("User connected")
-        socket.on('join', function(userNickname) {
+        socket.on('create', function(userNickname, room) {
+            socket.join(room)
             console.log(userNickname + " : has joined the chat ")
-            socket.broadcast.emit('userjoinedthechat', userNickname + " : has joined the chat")
+            io.sockets.in(room).emit('userjoinedthechat', userNickname + " : has joined the chat")
         })
 
-        socket.on('messagedetection', (senderNickname, messageContent) => {
+        socket.on('messagedetection', (senderNickname, messageContent, room) => {
             console.log(senderNickname + " : " + messageContent)
 
             let message = {
@@ -14,12 +14,12 @@ module.exports = (io) => {
                 "senderNickname": senderNickname
             }
 
-            io.emit('message', message)
+            io.sockets.in(room).emit('message', message)
         })
 
-        socket.on('disconnect', function() {
-            console.log('user has left')
-            socket.broadcast.emit("userdisconnect", ' user has left')
-        })
+        // socket.on('disconnect', function(userNickname, room) {
+        //     console.log(`${userNickname} has left`)
+        //     io.sockets.in(room).emit("userdisconnect", `${userNickname} user has left`)
+        // })
     })
 }
